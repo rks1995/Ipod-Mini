229bee5e09155379a8783baeca01fa059fb69bf0
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-08 14:26:01 +0530

    add rotate wheel feature using zingtouch library

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,17 +1,41 @@
 import React from 'react';
+import ZingTouch from 'zingtouch';
+
 class Wheel extends React.Component {
-  //   constructor() {
-  //     super();
-  //   }
+  constructor() {
+    super();
+    this.state = {
+      isClicked: true,
+    };
+  }
 
   handleWheelControls = (e) => {
-    console.log(e.clintY);
+    const wheelContainer = document.getElementById('wheel');
+    const region = ZingTouch.Region(wheelContainer);
+
+    var angle = 0;
+    region.bind(wheelContainer, 'rotate', function (e) {
+      console.log('Wheel');
+      var distance = e.detail.distanceFromLast;
+
+      //   if (distance < 0) {
+      //     angle++;
+      //     console.log(angle);
+      //   } else {
+      //     angle--;
+      //     console.log(angle);
+      //   }
+    });
   };
 
   render() {
     return (
       <div className='wheel-container'>
-        <div className='controls' onMouseMove={this.handleWheelControls}>
+        <div
+          id='wheel'
+          className='controls'
+          onMouseOver={this.handleWheelControls}
+        >
           <div className='menu'>MENU</div>
           <div className='start'></div>
           <div className='back'>
