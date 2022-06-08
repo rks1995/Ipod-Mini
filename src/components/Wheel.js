3330a351abdf8603ef79a01695f9d44b42caea8a
Senjam RatnaKumar <sratnakumar99@gmail.com>
2022-06-08 12:06:59 +0530

    add mousemove event when mouse enters control div for scrolling

--- src/components/Wheel.js
+++ src/components/Wheel.js
@@ -1,22 +1,28 @@
 import React from 'react';
-
 class Wheel extends React.Component {
+  //   constructor() {
+  //     super();
+  //   }
+
+  handleWheelControls = (e) => {
+    console.log(e.clintY);
+  };
+
   render() {
     return (
       <div className='wheel-container'>
-        <div className='controls'>
+        <div className='controls' onMouseMove={this.handleWheelControls}>
           <div className='menu'>MENU</div>
           <div className='start'></div>
           <div className='back'>
-            <i class='fa-solid fa-backward-fast'></i>
+            <i className='fa-solid fa-backward-fast'></i>
           </div>
           <div className='forward'>
-            <i class='fa-solid fa-forward-fast'></i>
+            <i className='fa-solid fa-forward-fast'></i>
           </div>
           <div className='play-pause'>
-            <i class='fa-solid fa-play'></i>
-            <i class='fa-solid fa-pause'></i>
-            <i class='fa-solid fa-play-pause'></i>
+            <i className='fa-solid fa-play'></i>
+            <i className='fa-solid fa-pause'></i>
           </div>
         </div>
       </div>
